eccc4f5cec0af3aaf5c884b135a50bbe667816e5
verena wahed <verowahed90@gmail.com>
2025-07-20 04:32:35 +0300

    Implement contect section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero'
 import Cocktials from './components/Cocktails'
 import About from './components/About'
 import Art from './components/Art'
+import Menu from './components/Menu'
+import Contact from './components/Contact'
   gsap.registerPlugin(ScrollTrigger, SplitText)
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <Cocktials/>
       <About/>
       <Art/>
+      <Menu/>
+      <Contact/>
     </main>
   )
 }
